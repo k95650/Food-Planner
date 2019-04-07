07d9df61565178d1bb6b244b08207bf077f3bc7c
k95650 <k95650@gmail.com>
2019-04-07 11:11:00 -0700

    Updates

---
+++ precache-manifest.c5d8acd4cd04a96fbb66c9e59beed0ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdb215c8e0217572b5fc",
+    "url": "/Food-Planner/static/css/main.37116cbb.chunk.css"
+  },
+  {
+    "revision": "fdb215c8e0217572b5fc",
+    "url": "/Food-Planner/static/js/main.ce6d8dd2.chunk.js"
+  },
+  {
+    "revision": "f2de84a5b06927ab2560",
+    "url": "/Food-Planner/static/js/runtime~main.59445e51.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Food-Planner/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Food-Planner/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0a1cb6c8440bde4053028130aca7521a",
+    "url": "/Food-Planner/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Food-Planner/precache-manifest.c5d8acd4cd04a96fbb66c9e59beed0ba.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Food-Planner/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
